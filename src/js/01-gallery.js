443ce05eae9beea80aac270418b64ae14f80a249
Andrii-Vekker <vekker304@gmail.com>
2022-07-19 22:02:03 +0300

    2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,34 @@
 // Add imports above this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector(".gallery");
+
+galleryRef.addEventListener("click", galleryHandler);
+
+function galleryHandler(e) {
+    e.preventDefault();
+      if (e.target.nodeName !== "IMG") {
+    return
+  };
+   const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+       captionPosition: "bottom",
+       showCounter: true,
+ enableKeyboard: true
+});
+};
+
+function createMurkup(items) {
+    return items.map(i => `<a class="gallery__item" href="${i.original}">
+  <img class="gallery__image" src="${i.preview}" alt="${i.description}" />
+</a>`).join("");
+};
+createMurkup(galleryItems)
+function insertGalleryItems(string) {
+    galleryRef.insertAdjacentHTML("beforeend", string);
+}
+insertGalleryItems(createMurkup(galleryItems));
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,11 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector("#vimeo-player");
+console.log(iframe.width)
+
+const player = new Player(iframe, {
+    id: iframe.id,
+    width: iframe.width
+});
 
