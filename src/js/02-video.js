a7a888ceffe6c500bfc7c1bba2beb168d7bc9ba8
Andrii-Vekker <65786577+Andrii-Vekker@users.noreply.github.com>
2022-07-20 16:39:24 +0300

    done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,10 +2,28 @@ import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector("#vimeo-player");
-console.log(iframe.width)
 
+const CURRENT_TIME = "videoplayer-current-time"
 const player = new Player(iframe, {
     id: iframe.id,
     width: iframe.width
 });
+function savinTimeFn (data) {
+    const value = data.seconds;
+    console.log(value)
+         localStorage.setItem(CURRENT_TIME, JSON.stringify(value));
+    };
+	player.on('timeupdate', throttle(savinTimeFn, 1000))
+const savedTime = localStorage.getItem(CURRENT_TIME);
 
+player.setCurrentTime(savedTime);
+//     player.setCurrentTime(savedTime).then(function(seconds) {
+// }).catch(function(error) {
+//     switch (error.name) {
+//         case 'RangeError':
+//             break;
+
+//         default:
+//             break;
+//     }
+// });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,38 @@
+import throttle from 'lodash.throttle';
 
+const FEEDBACK_KEY = "feedback-form-state";
+
+const refs = {
+    form: document.querySelector(".feedback-form"),
+    input: document.querySelector("input"),
+    textarea: document.querySelector("textarea"),
+    button: document.querySelector("button")
+};
+
+refs.form.addEventListener("input", throttle(handleInput, 500));
+refs.button.addEventListener("submit", handleSubmit);
+
+const formData = {};
+
+function handleInput(e) {
+    formData[e.target.name] = e.target.value;
+    const inputText = JSON.stringify(formData);
+    console.log(inputText)
+    localStorage.setItem(FEEDBACK_KEY, inputText);
+};
+
+function handleSubmit(e) {
+    e.preventDefault();
+    e.target.reset();
+    localStorage.removeItem(FEEDBACK_KEY);
+};
+
+function showText() {
+    const savedText = localStorage.getItem(FEEDBACK_KEY); 
+    const parcedSavedText = JSON.parse(savedText);
+    if (savedText) {
+        refs.input.value = parcedSavedText.email;
+        refs.textarea.value = parcedSavedText.message;
+    };
+};
+showText()
